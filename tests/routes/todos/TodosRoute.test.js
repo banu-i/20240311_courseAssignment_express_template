7482623cdf4e902f9f8b898641e059e6d5735773
banu-i <152266442+banu-i@users.noreply.github.com>
2024-03-13 13:17:14 +0100

    sync: 20240311 - Unterricht_testing branch _testing und supertest jest

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -2,12 +2,31 @@ const request = require("supertest");
 const app = require("../../../src/server");
 
 describe("GET /v1/todos/all", () => {
-  test("responds with json", async () => {
+  test("Test /all todos route", async () => {
     const response = await request(app)
       .get("/v1/todos/all")
       .expect("Content-Type", /json/)
       .expect(200);
 
-    expect(response.body).toEqual([]);
+    const myTodos = response.body;
+    const myFirstTodo = myTodos[0];
+
+    expect(myTodos.length).toBeGreaterThan(0);
+    expect(myFirstTodo.id).toBeDefined();
+    expect(myFirstTodo.task).toBeDefined();
+    expect(myFirstTodo.userId).toBeDefined();
+  });
+});
+
+describe("GET /v1/todos/byid:id?", () => {
+  test("GET by Id", async () => {
+    const todoId = 1;
+    const response = await request(app)
+      .get(`/v1/todos/byid?todoId=${todoId}`)
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    const myTodo = response.body.todo;
+    expect(myTodo.id).toEqual(todoId);
   });
 });
--- tests/setup/jest.setup.js
+++ tests/setup/jest.setup.js
@@ -1,5 +1,7 @@
 require("../../src/server");
+const TodoModel = require("../../src/database/models/TodoModel");
 const todoSequelize = require("../../src/database/setup/database");
+const TestDataTodos = require("./test-data/TestDataTodos");
 
 module.exports = async () => {
   try {
@@ -11,7 +13,7 @@ module.exports = async () => {
     await todoSequelize.dropSchema("Todos");
     await todoSequelize.sync();
     // DB mit Daten füllen, um DB auf Test Szenarien vorzubereiten
-    console.log("Test DB init");
+    await TodoModel.bulkCreate(TestDataTodos);
   } catch (e) {
     console.error("MY DB Issue", e);
   }
